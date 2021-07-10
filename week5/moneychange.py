50b44294e9f7a8457c00460e8f9eb6c1b3d94db2
sollunium <68354747+sollunium@users.noreply.github.com>
2021-07-10 17:58:55 -0400

    fixed

--- week5/moneychange.py
+++ week5/moneychange.py
@@ -9,28 +9,22 @@ def get_change(m):
     minimum(0)
     list = (m+1)*[0]
     list[0] = 0
+
     for i in range(1, m+1):
-        if i % 3 == 0:
-            list[i] = i//3
-            count = i
-        elif i % 4 == 0:
-            list[i] = i//4
-            count = i
+        if i >= 3:
+            if list[i-1] <= list[i-3] and list[i-1] <= list[i-4]:
+                list[i] = list[i-1] + 1
+
+            elif list[i-3] <= list[i-1] and list[i-3] <= list[i-4]:
+                list[i] = list[i-3] + 1
+
+
+            elif list[i-4] <= list[i-1] and list[i-4] <= list[i-3]:
+
+                list[i] = list[i - 4] + 1
 
         else:
-            if i > 3:
-                if list[i-1] <= list[i-3] or list[i-1] <= list[i-4]:
-                    list[i] = list[i-1]+ 1
-
-                elif list[i-3] <= list[i-1] or list[i-3] <= list[i-1]:
-                    list[i] = list[i-3] + 1
-
-
-                elif list[i-4] <= list[i-1] or list[i-4] <= list[i-1]:
-                    list[i] = list[i - 4] + 1
-
-            else:
-                list[i] = i
+            list[i] = i
 
     return list[m]
 
