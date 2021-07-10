16b44ea12b04706d4ad533c169ab7c8c8adf3050
sollunium <68354747+sollunium@users.noreply.github.com>
2021-07-10 19:05:05 -0400

    Calculator fixed

--- week5/Primitive Calculator.py
+++ week5/Primitive Calculator.py
@@ -16,11 +16,8 @@ def optimal_sequence(n):
         sequence.append(n)
         if n % 3 == 0:
 
-            if (n-1)% 3 == 0:
-                n -= 1
+            n = n // 3
 
-            else:
-                n = n // 3
         elif n % 2 == 0:
 
             if (n-1)% 3 == 0:
@@ -31,6 +28,7 @@ def optimal_sequence(n):
 
         else:
             n = n - 1
+
     return reversed(sequence)
 
 if __name__ == "__main__":
